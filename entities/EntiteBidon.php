637dd3fd8d408e2034dc8e974845cf2d1ebc8a0d
HugoCharles <usrlocal@Pcistiaetu87.ad.univ-angers.fr>
2013-10-18 11:53:35 +0200

    Ajout de EntiteBidon pour un test

---
+++ entities/EntiteBidon.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of EntiteBidon
+ *
+ * @author usrlocal
+ */
+class EntiteBidon {
+  //put your code here
+}
+
+?>
