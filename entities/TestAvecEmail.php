41412cb819a2ca75785baf922671e4a3f747812b
HugoY <usrlocal@Pcistiaetu87.ad.univ-angers.fr>
2013-10-18 12:06:47 +0200

    Test avec email

---
+++ entities/TestAvecEmail.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of TestAvecEmail
+ *
+ * @author usrlocal
+ */
+class TestAvecEmail {
+  //put your code here
+}
+
+?>
