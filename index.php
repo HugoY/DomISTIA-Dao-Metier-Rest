06ce2995de6ba1b5d04b672f73370eaa3a796ca2
HugoY <usrlocal@Pcistiaetu85.ad.univ-angers.fr>
2013-11-05 11:41:57 +0100

    debut thread

--- index.php
+++ index.php
@@ -3,13 +3,13 @@
 // port : 100
 // chaine renvoyer par Arduino {"id":"192.168.2.3","desc":"duemilanove","mac":"90:A2:DA:00:1D:A7","port":102}
 require_once 'dao/Recorder.php';
-$adress = "172.20.82.103";
+$adress = "172.20.82.172";
 $port = 10000;
 ini_set("max_execution_time",0);
 $serveur  = new Recorder();
 $serveur->init($adress, $port);
 //$serveur->run();
-$serveur->start();
+$serveur->start();//Will start a new Thread to execute the implemented run method 
 
 echo "Je m'affiche alors que run est dans une boucle infinie";
 ?>
