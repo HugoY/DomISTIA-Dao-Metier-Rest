000f94d96f5386da6a68b5b4b4dd425440cec040
Birot <julienbirot@laposte.net>
2013-11-12 11:15:05 +0100

    Ajout de l'interface metier

--- metier/IMetier.php
+++ metier/IMetier.php
@@ -8,9 +8,28 @@
 /**
  *
  * @author BIROT
+ * 
+ * public interface IMetier {
+// liste des arduinos
+public List<Arduino> getArduinos();
+// lecture d'une pin
+public Reponse pinRead(String idCommande, String idArduino, int pin, String mode);
+// écriture d'une pin
+public Reponse pinWrite(String idCommande, String idArduino, int pin, String mode,int val);
+// faire clignoter une led
+public void faireClignoterLed(String idCommande, String idArduino, int pin, int millis, int nbIter);
+// envoyer une suite de commandes Json à un Arduino
+public List<String> sendCommandesJson(String idArduino, List<String> commandes);
+// envoyer une suite de commandes à un Arduino
+public List<Reponse> sendCommandes(String idArduino, List<Commande> commandes);
+}
  */
 interface IMetier {
     //put your code here
+    public function getArduino();
+    public function pinRead($idCommande, $idArduino,$pin,$mode);
+
+
 }
 
 ?>
