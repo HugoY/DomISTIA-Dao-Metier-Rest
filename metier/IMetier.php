4afd88c10540497a48a7ed0eac28f0d907a940a6
Birot <julienbirot@laposte.net>
2013-11-12 11:08:31 +0100

    Ajout de l'interface metier

--- metier/IMetier.php
+++ metier/IMetier.php
@@ -4,4 +4,13 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
+
+/**
+ *
+ * @author BIROT
+ */
+interface IMetier {
+    //put your code here
+}
+
 ?>
