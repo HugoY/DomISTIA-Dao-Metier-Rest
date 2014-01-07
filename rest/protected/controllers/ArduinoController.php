42ad59cd85b0b9eaf51e86f42d7ca8f6980b87ce
Birot <julienbirot@laposte.net>
2014-01-07 17:04:04 +0100

    commit avant merge

--- rest/protected/controllers/ArduinoController.php
+++ rest/protected/controllers/ArduinoController.php
@@ -2,12 +2,12 @@
 
 require_once(dirname(__FILE__) . '/../../../metier/MetierSimulation.php');
 //require_once(dirname(__FILE__) . '/../../../metier/Metier.php');
-require_once (dirname(__FILE__) .'/../../../entities/DomotiqueException.php');
+require_once (dirname(__FILE__) . '/../../../entities/DomotiqueException.php');
 
 class ArduinoController extends CController {
 
   private $metier;
-  
+
   protected function beforeAction($action) {
     $this->metier = MetierSimulation::getInstance();
     return true;
@@ -17,14 +17,13 @@ class ArduinoController extends CController {
     echo 'actionIndex';
   }
 
-
   public function actionArduinos() {
     // Couche métier
-     //sleep(5);
+    //sleep(5);
     $arduinoArray = array();
 
-    while(count($this->metier->getArduinos())==0){
-        sleep(1);
+    while (count($this->metier->getArduinos()) == 0) {
+      sleep(1);
     }
     foreach ($this->metier->getArduinos() as $a) {
       $arduinoArray[] = $a->toArray();
@@ -34,73 +33,67 @@ class ArduinoController extends CController {
   }
 
   public function actionBlink() {
-        
-      while(count($this->metier->getArduinos())==0){
-        sleep(1);
-    }
-        //var_dump($metier->getArduinos());
-        try{
-          $this->metier->faireClignoterLed($_GET['idCommand'] , $_GET['ip'], $_GET['pin'], $_GET['duree'],$_GET['nbIter']);
-          $reponse=array("id"=>$_GET['idCommand'],
-                          "erreur"=>"0",
-                          "etat"=> array(),
-                          "json"=>NULL             
-              
-              );
-          $reponseArray=array("data"=>$reponse);
-        }catch(Exception $e){
-          $reponseArray=array("message"=>$e->getMessage(),
-                          "erreur"=>$e->getCode()
-              
-              );
-        }
-        
-        echo json_encode($reponseArray);
-       
-    }
-    
-    public function actionRead() {
-        
-        
-       
-        try{
-          $reponse=$this->metier->pinRead($_GET['idCommand'] , $_GET['ip'], $_GET['pin'], $_GET['mode']);
-          $reponse2= array("id"=>$reponse->getId(),
-                            "erreur"=>$reponse->getErreur(),
-                          "etat"=> $reponse->getEtat(),
-                          "json"=>NULL   
-              );
-          $reponseArray=array("data"=>$reponse2);
-        }catch(Exception $e){
-          $reponseArray=array("message"=>$e->getMessage(),
-                          "erreur"=>$e->getCode()
-              
-              );
-        }
-       
-        echo json_encode($reponseArray);
 
-}
-public function actionWrite() {
-        
-        
-       
-        try{
-          $reponse=$this->metier->pinWrite($_GET['idCommand'] , $_GET['ip'], $_GET['pin'], $_GET['mode'],$_GET['valeur']);
-          $reponse2= array("id"=>$reponse->getId(),
-                            "erreur"=>$reponse->getErreur(),
-                          "etat"=> $reponse->getEtat(),
-                          "json"=>NULL   
-              );
-          $reponseArray=array("data"=>$reponse2);
-        }catch(Exception $e){
-          $reponseArray=array("message"=>$e->getMessage(),
-                          "erreur"=>$e->getCode()
-              
-              );
-        }
-        
-        echo json_encode($reponseArray);
+    while (count($this->metier->getArduinos()) == 0) {
+      sleep(1);
+    }
+    //var_dump($metier->getArduinos());
+    try {
+      $this->metier->faireClignoterLed($_GET['idCommand'], $_GET['ip'], $_GET['pin'], $_GET['duree'], $_GET['nbIter']);
+      $reponse = array("id" => $_GET['idCommand'],
+          "erreur" => "0",
+          "etat" => array(),
+          "json" => NULL
+      );
+      $reponseArray = array("data" => $reponse);
+    } catch (Exception $e) {
+      $reponseArray = array("message" => $e->getMessage(),
+          "erreur" => $e->getCode()
+      );
+    }
 
-}
+    echo json_encode($reponseArray);
+  }
+
+  public function actionRead() {
+
+
+    try {
+      $reponse = $this->metier->pinRead($_GET['idCommand'], $_GET['ip'], $_GET['pin'], $_GET['mode']);
+      $reponse2 = array("id" => $reponse->getId(),
+          "erreur" => $reponse->getErreur(),
+          "etat" => $reponse->getEtat(),
+          "json" => NULL
+      );
+      $reponseArray = array("data" => $reponse2);
+    } catch (Exception $e) {
+      $reponseArray = array("message" => $e->getMessage(),
+          "erreur" => $e->getCode()
+      );
+    }
+
+    echo json_encode($reponseArray);
+  }
+
+  public function actionWrite() {
+
+
+
+    try {
+      $reponse = $this->metier->pinWrite($_GET['idCommand'], $_GET['ip'], $_GET['pin'], $_GET['mode'], $_GET['valeur']);
+      $reponse2 = array("id" => $reponse->getId(),
+          "erreur" => $reponse->getErreur(),
+          "etat" => $reponse->getEtat(),
+          "json" => NULL
+      );
+      $reponseArray = array("data" => $reponse2);
+    } catch (Exception $e) {
+      $reponseArray = array("message" => $e->getMessage(),
+          "erreur" => $e->getCode()
+      );
+    }
+
+    echo json_encode($reponseArray);
+  }
+
 }
